aa8dcabcd3ff336930649103f42356f3506e37a6
Marshall Davis <marmadav@gmail.com>
2020-02-25 10:44:19 -0500

    Add project files.

---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class RestaurantDbContext:DbContext//Inherited from Entity framework, allows to communicate with database
+    {
+        public RestaurantDbContext() :base("DefaultConnection") //Calls constructor 
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
